1ff3f6d15aaac6a26ec976425098271792e97b7b
Your Name <you@example.com>
2021-01-24 16:10:14 -0500

    added create, index, and show routes for users controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: UserSerializer.new(users).to_serialized_json
+    end
+
+    def show
+        user = User.find_by(params[:id])
+        render json: UserSerializer.new(user).to_serialized_json
+    end
+
+    def create
+        user = User.new(user_params)
+        user.save
+        render json: user
+    end
+
+    private
+
+    def user_params 
+        params.require(:user).permit(:name)
+    end
 end
