cab3da99feae5851eb39bf4b4e2c31b4f1083c70
Your Name <you@example.com>
2021-01-28 12:24:07 -0500

    like button/reset button works successfully.

--- frontend/javascripts/models/recipe.js
+++ frontend/javascripts/models/recipe.js
@@ -2,7 +2,7 @@ class Recipe {
 
     static all = []
 
-    constructor(title, status='', cook_time, prep_time, directions, id) {
+    constructor(title, status='Incomplete', cook_time, prep_time, directions, id) {
         this.title = title
         this.status = status
         this.cook_time = cook_time
@@ -15,7 +15,7 @@ class Recipe {
     static postRecipe(recipeData) {
         let formData = {
             "title": recipeData.title.value,
-            "status": recipeData.status = '',
+            "status": recipeData.status = 'Incomplete',
             "cook_time": recipeData.cook_time.value,
             "prep_time": recipeData.prep_time.value,
             "directions": recipeData.directions.value,
@@ -46,6 +46,7 @@ class Recipe {
         let h2 = document.createElement('h2')
         h2.innerHTML = `<strong>${this.title}</strong>`
 
+
         let h4 = document.createElement('h4')
         h4.innerHTML = `<strong>Cook Time: ${this.cook_time} minutes<br>Prep Time: ${this.prep_time} minutes</strong>`
         
@@ -55,34 +56,34 @@ class Recipe {
         let p = document.createElement('p')
         p.setAttribute('class', 'recipe-status')
         p.innerHTML = `${this.status}`
-        
+
+        let likeBtn = document.createElement('button')
+        likeBtn.setAttribute('class', 'like-recipe-btn')
+        likeBtn.innerText = 'Like'
+        likeBtn.addEventListener('click', event => this.likeHandler(event, this))
+
+        let resetBtn = document.createElement('button')
+        resetBtn.setAttribute('class', 'reset-recipe-btn')
+        resetBtn.innerText = 'Reset'
+        resetBtn.addEventListener('click', event => this.resetHandler(event, this))
+
+        if (p.innerHTML === 'Incomplete') {
+            p.style.color = 'red'
+            resetBtn.style.display = 'none'
+        } else {
+            p.style.color = 'green'
+            likeBtn.style.display = 'none'
+        }
+
         let deleteBtn = document.createElement('button')
         deleteBtn.setAttribute('class', 'delete-recipe-btn')
         deleteBtn.innerText = 'Delete'
         deleteBtn.addEventListener('click', event => this.deleteRecipeHandler(event, this))
-        
-        let unlikeBtn = document.createElement('button')
-        unlikeBtn.setAttribute('class', 'unlike-recipe-btn')
-        unlikeBtn.innerHTML = 'Unlike!'
-        unlikeBtn.addEventListener('click', event => this.unlikeHandler(event, this))
-
-        let likeBtn = document.createElement('button')
-        likeBtn.setAttribute('class', 'like-recipe-btn')
-        likeBtn.innerHTML = 'Like'
-        likeBtn.addEventListener('click', event => this.likeHandler(event, this))
-
-        if (p.innerHTML === 'Not liked') {
-            p.style.color = 'red'
-            likeBtn.style.display = 'none'
-        } else {
-            p.style.color = 'green'
-            unlikeBtn.style.display = 'none'
-        }
 
         let divCard = document.createElement('div')
         divCard.setAttribute('class', 'card')
         divCard.setAttribute('id', `${this.id}`)
-        divCard.append(h2, h4, tbody, p, unlikeBtn, likeBtn, deleteBtn)
+        divCard.append(h2, h4, tbody, p, likeBtn, resetBtn, deleteBtn)
         recipeCollection.append(divCard)
     }
 
@@ -104,16 +105,16 @@ class Recipe {
         })
     }
 
-    unlikeHandler() {
+    likeHandler() {
         let cardIns = event.target.parentNode
-        cardIns.querySelector('.like-recipe-btn').style.display = 'block'
+        cardIns.querySelector('.reset-recipe-btn').style.display = 'block'
         event.preventDefault()
 
-        let toggleLikeBtn = event.target.style.display = 'none'
+        let toggleResetBtn = event.target.style.display = 'none'
 
-        let likeUpdate = event.target.previousElementSibling.previousElementSibling
-        likeUpdate.innerHTML = 'Liked!'
-        likeUpdate.style.color = 'green'
+        let statusUpdate = event.target.previousElementSibling
+        statusUpdate.innerHTML = 'Liked!'
+        statusUpdate.style.color = 'green'
 
         fetch(`${Api.RECIPES_URL}/${this.id}`), {
             method: "PATCH",
@@ -122,39 +123,40 @@ class Recipe {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                'status': likeUpdate.textContent
+                'status': statusUpdate.textContent
             })
         }
         .then(parseJSON)
         .then(newStatus => {
-            likeUpdate
+            statusUpdate
         })
     }
 
-    likeHandler() {
-        let likeStatus = event.target.previousElementSibling.previousElementSibling
-        likeStatus.innerHTML = 'Not liked'
-        likeStatus.style.color = 'none'
-
-        let toggleUnlikeBtn = event.target.previousElementSibling
-        toggleUnlikeBtn.style.display = 'block'
-
-        let toggleLikeBtn = event.target.style.display = 'none'
+    resetHandler() {
+        let resetStatus = event.target.previousElementSibling.previousElementSibling
+        resetStatus.innerHTML = 'Incomplete'
+        resetStatus.style.color = 'red'
+    
+        let toggleLikeBtn = event.target.previousElementSibling
+        toggleLikeBtn.style.display = 'block'
+    
+        let toggleResetBtn = event.target.style.display = 'none'
         event.preventDefault()
-
-        fetch(`${Api.RECIPES_URL}/${this.id}`), {
+    
+        fetch(`${Api.RECIPES_URL}/${this.id}`, {
             method: "PATCH",
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                'status': likeStatus.textContent
+                'status': resetStatus.textContent
             })
-        }
+        })
         .then(parseJSON)
         .then(newStatus => {
-            likeStatus
+            resetStatus
         })
+        
     }
 }
